683ae051346ef5916ac8eb5e7d50827797c02b54
thijsrijkers <thijsrijkers@live.nl>
2022-01-31 22:55:44 +0100

    +Added some css to the card parser (Not final)

--- scripts/w_parser.ts
+++ scripts/w_parser.ts
@@ -5,7 +5,7 @@ module.exports =
         switch (true) 
         {
             case parse.indexOf("<w-card>") !== -1:
-                return parse.replace("<w-card>", "<div>");
+                return parse.replace("<w-card>", "<div style='width:100%; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);'>");
             case parse.indexOf("</w-card>") !== -1:
                 return parse.replace("</w-card>", "</div>");
             default:
