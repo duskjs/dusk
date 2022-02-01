2a85c43f33d4dd425b16cc8827fa24d952024e7c
Thijs.R <43141993+thijsrijkers@users.noreply.github.com>
2022-02-01 09:43:28 +0100

    Update server.ts

--- lib/app/server.ts
+++ lib/app/server.ts
@@ -10,7 +10,7 @@ module.exports = {
     RunServer: function () {
         var file = fs.readFileSync(path.join(__dirname, '../../dist/index.html'), 'utf-8');
 
-        JSON.parse(fs.readFileSync('metadep.json', 'utf8'))['component'].forEach((component:String) => file += fs.readFileSync(path.join(__dirname, '../../src/component/'+component+'/'+component+'.html'), 'utf-8'));
+        JSON.parse(fs.readFileSync('metadep.json', 'utf8'))['component'].forEach((component:String) => file += fs.readFileSync(path.join(__dirname, '../../dist/component/'+component+'/'+component+'.html'), 'utf-8'));
        
         const server = http.createServer(function(request:any, response:any) {  
             response.writeHeader(200, {"Content-Type": "text/html"});  
